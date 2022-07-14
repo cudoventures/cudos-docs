acc0e7d0f11b695ce1992742b64b357b9f731870
Anita Diamond <38940580+ddocs@users.noreply.github.com>
2022-07-14 09:20:50 +0100

    removed blog links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,13 +38,6 @@ const config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
@@ -90,19 +83,6 @@ const config = {
               {
                 label: 'Medium',
                 href: 'https://medium.com/cudos'
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
               },
             ],
           },
