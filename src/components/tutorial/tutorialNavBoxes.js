b227399514dc91fc3a0e5106f7bd0c8e8d8de2b0
Anita Diamond <38940580+ddocs@users.noreply.github.com>
2023-01-26 11:16:00 +0000

    fixing reload css issue

--- src/components/tutorial/tutorialNavBoxes.js
+++ src/components/tutorial/tutorialNavBoxes.js
@@ -6,7 +6,7 @@ import styles from './tutorialNavBoxes.module.css';
 const FeatureList = [
   {
     title: 'Build a simple poll',
-    icon: 'img/rocket.png',
+    icon: 'rocket.png',
     items: [
       {
       url: "docs/build/tutorials/simple-poll/build-poll", 
@@ -16,14 +16,14 @@ const FeatureList = [
   },
   {
     title: 'Build a messaging dApp',
-    icon: 'img/messaging.svg',
+    icon: 'messaging.svg',
     items: [
       {url: "docs/build/tutorials/messaging/create-messaging", text: "Get Started"},
     ]
   },
   {
     title: 'Build a Counter with Cudos Blast',
-    icon: 'img/nft.svg',
+    icon: 'nft.svg',
     items: [
       {url: "docs/build/tutorials/counter/create-counter", text: "Get Started"},
     ]
