127035966e365a3630bd74585f140f71492ec44f
jgtormo <joan.garciaitormo@cudoventures.com>
2024-11-15 11:58:23 +0000

    remove legacy docs

--- docs/welcome.md
+++ docs/welcome.md
@@ -24,9 +24,3 @@ CUDOS is a leading DePIN project that offers sustainable high performance comput
 CUDOS is part of Cudo Ventures, the company behind [Cudo Compute](https://www.cudocompute.com/), the high performance GPU cloud that CUDOS Intercloud gets most of its resources from.
 
 While CUDOS used to have an ERC-20 token and its own Cosmos chain, it [merged both of this with the Artificial Superintelligence Alliance](https://x.com/CUDOS_/status/1839754601920376851) (ASI Alliance), and is now part of the bigger ASI ecosystem as an Alliance member.
-
-## More resources
-
-Below there are links to some legacy documentation and tutorials that might still be useful for aspiring Cosmos developers and enthusiasts.
-- [Legacy docs](/docs/legacy).
-- [Legacy tutorials](/tutorials).
--- sidebars.js
+++ sidebars.js
@@ -19,62 +19,6 @@ module.exports = {
             'asi-merge/final-cudos-data',
           ]
         },
-        'legacy',
-      ],
-    },
-  ],
-  nodeSidebar: [
-    {
-      type: 'category',
-      label: 'Overview',
-      items: [
-        'node/overview/understanding-nodes',
-        'node/overview/delegators',
-        'node/overview/validator-mechanics',
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Security',
-      items: [
-        'node/security/key-management',
-        'node/security/keys',
-        'node/security/sentry-node-arch'
-      ],
-    },
-  ],
-
-  buildSidebar: [
-    {
-      type: 'category',
-      label: 'Overview',
-      items: [
-        'build/overview/setup-rust',
-      ]
-    },
-    {
-      type: 'category',
-      label: 'Smart Contracts',
-      items: [
-        'build/smart-contracts/contract-basics',
-        'build/smart-contracts/contract-deployment',
-        'build/smart-contracts/contract-execution',
-        'build/smart-contracts/contract-messages',
-        'build/smart-contracts/contract-state',
-        'build/smart-contracts/contract-entrypoints',
-      ],
-    },
-  ],
-
-  learnSidebar: [
-    {
-      type: 'category',
-      collapsed: false,
-      label: 'Concepts',
-      items: [
-        'learn/concepts/token-types',
-        'learn/concepts/transactions',
-        'learn/concepts/cryptography',
       ],
     },
   ],
@@ -119,35 +63,6 @@ module.exports = {
         'cudos-intercloud/support/general-support',
         'cudos-intercloud/support/faqs',
       ],
-    },
-  ],
-
-  pollTutorialSidebar: [
-    {
-      type: 'category',
-      label: 'Simple Poll',
-      items: [
-        'build/tutorials/simple-poll/build-poll', 'build/tutorials/simple-poll/modify-state', 'build/tutorials/simple-poll/modify-schema', 'build/tutorials/simple-poll/modify-contract', 'build/tutorials/simple-poll/modify-executemsg', 'build/tutorials/simple-poll/execute', 'build/tutorials/simple-poll/querymsg', 'build/tutorials/simple-poll/query',
-      ]
-    }
-
-  ],
-
-  counterTutorialSidebar: [
-    {
-      type: 'category',
-      label: 'Counter',
-      items: [
-        'build/tutorials/counter/create-counter', 'build/tutorials/counter/compile-contract', 'build/tutorials/counter/connect-node', 'build/tutorials/counter/deploy-counter', 'build/tutorials/counter/interact-counter'
-      ],
-    },
-  ],
-
-  messagingTutorialSidebar: [
-    {
-      type: 'category',
-      label: 'MessagingdApp',
-      items: ['build/tutorials/messaging/create-messaging', 'build/tutorials/messaging/compile-messaging', 'build/tutorials/messaging/connect-messaging-node', 'build/tutorials/messaging/deploy-messaging'],
     },
   ],
 };
